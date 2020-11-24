7adeb71b3a30fb50f5b3aaf717289a1f10b44aa7
Aamir Soni <aamirsoni1551@gmail.com>
2020-11-24 19:25:36 +0000

    first commit

---
+++ d2/j.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <hh.hpp>
+
+/*int main()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}*/
+
+int a()
+{
+    return f() * 123;
+}
---
+++ m.cpp
@@ -0,0 +1,10 @@
+#include <ii.hpp>
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << a() << endl;
+
+    return 0;
+}
